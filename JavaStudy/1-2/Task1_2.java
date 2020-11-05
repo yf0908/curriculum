cb8f251974acfcfe44c0033733eb0a0c403aa59c
yuji fujita <72638183+yf0908@users.noreply.github.com>
2020-11-05 20:40:49 +0900

    task1-2

--- JavaStudy/1-2/Task1_2.java
+++ JavaStudy/1-2/Task1_2.java
@@ -12,18 +12,23 @@ public class Task1_2 {
   public static void main(String[] args) {
 
       // ①「JAPAN」、「AMERICA」、「KOREA」を要素の値（初期値）とする配列 countryを作成しなさい。
-      String[] array = new String[3];
+      // 宣言と初期化を同時に行う
       String[] country = {"JAPAN", "AMERICA", "KOREA"};
       
       // ② ①で作成した配列の要素数を出力してください。
-      System.out.println("配列の要素数は" + array.length);
+      System.out.println("配列の要素数は" + country.length);
 
       /* ③下記の値を保持した、要素数3のStringクラスの配列strArrayを作成しなさい。
        *   1番目（先頭）の要素に 「りんご」 を代入
        *   2番目の要素に 「もも」 を代入
        *   3番目の要素に 「ぶどう」 を代入
        */
-      String[] strArray = {"りんご", "もも", "ぶどう"};
+       // String[] strArray = {"りんご", "もも", "ぶどう"};
+       // 宣言と初期化の別々に記述する
+       String[] strArray = new String[3];
+      strArray[0] = "りんご";
+      strArray[1] = "もも";
+      strArray[2] = "ぶどう";
 
       // ④ ③で作成した配列の2番目の要素を出力しなさい。
       System.out.println(strArray[1]);
@@ -31,7 +36,7 @@ public class Task1_2 {
       /* ⑤下記の処理について、何をしているのかコメントを記入してください。
        * 配列に整数型のクラスを付与してスコア（要素）を格納
        */
-      int[] intArray = { 10, 20, 30, 40, 50};
+      int[] intArray = {10, 20, 30, 40, 50};
 
       // ⑥下記の処理について、何をしているのかコメントを記入してください。
       // 配列に格納されている2番目と5番目のスコア（要素）の足し算
@@ -39,3 +44,4 @@ public class Task1_2 {
 
   }
 }
+
