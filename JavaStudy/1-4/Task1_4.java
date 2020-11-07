05474546a2a459e5ca4ecf00cc3c6e25f5647cc9
yuji fujita <72638183+yf0908@users.noreply.github.com>
2020-11-07 13:26:44 +0900

    Task1-4

--- JavaStudy/1-4/Task1_4.java
+++ JavaStudy/1-4/Task1_4.java
@@ -27,28 +27,36 @@ public class Task1_4 {
   public static void main(String[] args) {
     // 以下の変数「name（名前）」「pass（パスワード）」を使用して、②〜⑤の条件を満たす処理を記述してください。
     // 名前とパスワードは画面からの入力値だと想定してみましょう。
-    String name = "alice";
-    String pass = "pass";
+    String name = "alic";
+    String pass = "pas";
     // ② 名前が「 alice 」、パスワードが「 pass 」であった場合、「 ログイン成功です 」と出力してください。
-    if (name.equals("alice") && pass.equals("pass")) {
-      System.out.println(CONST_MSG_SUCCESS);
-    }
+    // if (name.equals("alice") && pass.equals("pass")) {
+    //   System.out.println(CONST_MSG_SUCCESS);
+    // }
 
     // ③ 名前のみ正しい場合、「 パスワードに誤りがあります。 」と出力してください。
-    if (name.equals("alice") != pass.equals("pass")) {
+    if (name.equals("alice") && name.equals("pass")) {
+      System.out.println(CONST_MSG_SUCCESS);
+    } else if (name.equals("alice")) {
       System.out.println(CONST_MSG_ERROR_PASS);
-    }
+    } else {
 
-    // ④ パスワードのみ正しい場合、「 名前に誤りがあります。 」と出力してください。
-    if (pass.equals("pass") != name.equals("alice")) {
-      System.out.println(CONST_MSG_ERROR_INPUT);
     }
-
-    // ⑤ 名前もパスワードも間違っていた場合は、「 入力情報に誤りがあります。 」と出力してください。
-    if (pass.equals("pass") ^ name.equals("alice")) {
-      System.out.println(CONST_MSG_ERROR_INPUT);
-    }
-
+    
+    
+    // // ④ パスワードのみ正しい場合、「 名前に誤りがあります。 」と出力してください。
+    // if (name.equals("alice") && name.equals("alice")) {
+    //   System.out.println();
+    // } else {
+    //   if (name.equals("pass")) {
+    //     System.out.println(CONST_MSG_ERROR_PASS);
+    //   }
+    // }
+    
+    // // ⑤ 名前もパスワードも間違っていた場合は、「 入力情報に誤りがあります。 」と出力してください。
+    // if (pass.equals("pass") ^ name.equals("alice")) {
+      //   System.out.println(CONST_MSG_ERROR_INPUT);
+      // }
+      
   }
-
 }
