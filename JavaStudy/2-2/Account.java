8a6a492c292f244501483ea2b6b7996d0b0a423c
yuji fujita <72638183+yf0908@users.noreply.github.com>
2020-11-13 07:36:07 +0900

    Task2-2

--- JavaStudy/2-2/Account.java
+++ JavaStudy/2-2/Account.java
@@ -12,18 +12,30 @@ public class Account {
 
     // コンストラクタ
     public Account(String userName, int id, String password) {
-        this.userName = "hoge";
-        this.id = 123;
-        this.password = "abc123";
+        this.userName = userName;
+        this.id = id;
+        this.password = password;
     }
 
     // ① 以下のルールに従いアカウント情報（名前、ID、パスワード）を出力するメソッドを作成してください。
     // アクセス修飾子:「protected」
     // メソッド名:「printAccountInfo」
-    protected void printAccountInfo() {
-        System.out.println(this.userName);
-        System.out.println(this.id);
-        System.out.println(this.password);
+    protected void printAccountInfo(String userName, int id, String password) {
+        System.out.println("ユーザー名は、" + userName);
+        System.out.println("IDは、" + id);
+        System.out.println("パスワードは、" + password);
     }
 
 }
+    /** ===メモ===
+    * コンストラクタには以下のルール・特徴がある
+    * ・コンストラクタの定義において戻り値の情報は何も記述しない
+    *     ※voidすら不要
+    * ・コンストラクタ名はクラス名と同じ
+    *     ※メソッド名のルールを外れ、大文字からの命名となる
+    * ・インスタンス生成時、必ず最初に呼び出される
+    */
+
+/**
+ * コンストラクタはメソッドの一種なので、様々な因数パターンに応じてオーバーロードしてい定義することが可能
+ */
