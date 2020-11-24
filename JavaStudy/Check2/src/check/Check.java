eb2afcbe43a7b8d295b6ea25eb965b3daeed1c19
yuji fujita <72638183+yf0908@users.noreply.github.com>
2020-11-24 20:32:00 +0900

    Check2

--- JavaStudy/Check2/src/check/Check.java
+++ JavaStudy/Check2/src/check/Check.java
@@ -1,8 +1,8 @@
 package check;
 
 import static java.lang.System.*;
+
 import constants.Constants;
-import check.Pet;
 
 public class Check {
 
@@ -11,16 +11,17 @@ public class Check {
 
     public static void main(String[] args) {
         out.println(printName("printNameメソッド → " + firstName, lastName));
+
+    Pet instance1 = new Pet(Constants.CHECK_CLASS_JAVA, Constants.CHECK_CLASS_HOGE);
+    instance1.introduce();
+
+    RobotPet instance2 = new RobotPet(Constants.CHECK_CLASS_R2D2, Constants.CHECK_CLASS_LUKE);
+    instance2.introduce();
+
     }
 
     private static String printName(String firstName, String lastName) {
         return firstName + lastName;
     }
-
-    Pet instance1 = new Pet(Constants.CHECK_CLASS_JAVA, Constants.CHECK_CLASS_HOGE);
-        instance1.introduce();
-
-    RobotPet instance2 = new RobotPet(Constants.CHECK_CLASS_LUKE, Constants.CHECK_CLASS_LUKE);
-        instance2.introduce();
 
 }
