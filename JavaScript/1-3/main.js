f3bcd479b6078d9ea6fb0522cd02096466c036f2
yuji fujita <72638183+yf0908@users.noreply.github.com>
2021-04-07 21:28:01 +0900

    task1-3

---
+++ JavaScript/1-3/main.js
@@ -0,0 +1,21 @@
+//問1
+let a = 2;
+let b = 12;
+console.log(a != 10);
+console.log(b >=10 && b<20);
+
+//問2
+let x = 10;
+if (x >= 10 && x <= 20 ) {
+  console.log("成功です");
+} else {
+  console.log("失敗です");
+}
+
+//問3
+let y = 0;
+if (y % 2 === 0) {
+  console.log("偶数です")
+} else {
+  console.log("奇数です")
+}
