66d62344986d1593800f81b8118466075d9a66e7
yuji fujita <72638183+yf0908@users.noreply.github.com>
2021-04-11 22:12:27 +0900

    task1-4

--- JavaScript/1-4/main.js
+++ JavaScript/1-4/main.js
@@ -1,5 +1,7 @@
 for (let number = 1; number <= 100; number++) {
-  if (number % 3 === 0) {
+  if (number % 3 === 0 && number % 5 === 0) {
+    console.log("Fizz!Buzz!!");
+  } else if (number % 3 === 0) {
     console.log("Fizz!");
   } else if (number % 5 === 0) {
     console.log("Buzz!");
