070b1fafd874351d098eccaf53eed3ce21c1cc61
yuji fujita <72638183+yf0908@users.noreply.github.com>
2021-04-18 21:16:21 +0900

    task2-1

---
+++ JavaScript/2-1/main.js
@@ -0,0 +1,9 @@
+function myfunc() {
+
+    // idが「box」の要素を取得して、変数ChangeBoxColorに代入
+    let ChangeBoxColor = document.getElementById("box");
+    
+    // style.backgroundColorを使って色を変更するで書き変える
+    ChangeBoxColor.style.backgroundColor = "red";
+
+}
