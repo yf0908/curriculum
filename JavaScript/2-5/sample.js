299725ce5aceaf34a40f11d61c69f042ddebd6b4
yuji fujita <72638183+yf0908@users.noreply.github.com>
2021-05-08 14:18:14 +0900

    chack2

---
+++ JavaScript/2-5/sample.js
@@ -0,0 +1,11 @@
+<script type="text/javascript">
+    function entryChange1(){
+        radio = document.getElementsByName('●●●') 
+
+        if(radio[0].checked) {
+        document.getElementById('firstBox').style.display = "";
+        } else {
+        document.getElementById('firstBox').style.display = "none";
+        }
+    }
+    </script>
